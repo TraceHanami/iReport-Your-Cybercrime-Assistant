d9d91599175acefc28c5b890df4705915656a479
Devikrishnakumar <devikrishnakumar07@gmail.com>
2025-11-04 11:15:05 +0530

    Delete backend directory

--- backend/open.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('complaints.db')
-c = conn.cursor()
-c.execute("SELECT * FROM complaints ORDER BY id DESC LIMIT 5")  # show last 5 complaints
-rows = c.fetchall()
-for row in rows:
-    print(row)
-conn.close()
